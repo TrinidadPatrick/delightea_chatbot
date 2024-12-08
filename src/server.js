4b0a590c4ff9bc354aaa5e4367dd08a0aedad27e
John Patrick Trinidad <Ptrinidad765@gmail.com>
2024-12-08 09:46:00 +0800

    upated

--- src/server.js
+++ src/server.js
@@ -10,13 +10,21 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use((req, res, next) => {
+    res.setHeader(
+      "Content-Security-Policy",
+      "default-src 'self'; script-src 'self' https://vercel.live; connect-src 'self' https://vercel.live; style-src 'self';"
+    );
+    next();
+  });
 //config view engine
 configViewEngine(app);
 
 //config web routes
 initWebRoutes(app);
 
-let port = process.env.PORT || 8081;
+let port = process.env.PadORT || 8081;
 
 app.listen(port, () => {
     console.log(`Messenger bot is running at the port ${port}`);
